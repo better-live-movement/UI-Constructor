657206da4df976af7f0f686cfa9494c47be2902d
Melchior Kannengießer <melchior.kannengiesser@gmx.de>
2017-06-05 14:23:39 +0200

    make the script react-ready

--- uc.sh
+++ uc.sh
@@ -26,8 +26,13 @@ DESCRIPTION
         show this help text
     -v
         show the script-version
+    -r
+        install reactjs
 "
 # --- Options processing -------------------------------------------
+redux=false
+longInit=false
+
 if [ $# == 0 ] ; then
     echo $USAGE >&2
     echo $EXAMPLE >&2
@@ -46,6 +51,14 @@ while getopts ":i:vh" optname
       "h")
         echo "$HELP"
         exit 0;
+        ;;
+      "r")
+        echo "Install redux"
+        redux=true
+        ;;
+      "l")
+        echo "long init"
+        long=true
         ;;
       "?")
         echo "Unknown option $OPTARG" >&2
@@ -67,48 +80,108 @@ param2=$2
 # --- Locks -------------------------------------------------------
 LOCK_FILE=/tmp/$SUBJECT.lock
 if [ -f "$LOCK_FILE" ]; then
-   echo "Script is already running"
-   exit
+  echo "Script is already running"
+  exit
 fi
 trap "rm -f $LOCK_FILE" EXIT
 touch $LOCK_FILE
 # --- Body --------------------------------------------------------
-#  SCRIPT LOGIC GOES HERE
 echo $param1
 echo $param2
+# --- Initialize npm project --------------------------------------
 mkdir $param1 && cd $param1
-npm init -y
-npm install --save-dev webpack
-npm install --save lodash
+if [ "$longInit" == false ]; then
+  touch package.json
+  echo '{' >> package.json
+  echo '  "name": "'$param1'",' >> package.json
+  echo '  "version": "0.1.0",' >> package.json
+  echo '  "description": "",' >> package.json
+  echo '  "scripts": {' >> package.json
+  echo '    "start": "npm run build",' >> package.json
+  echo '    "build": "webpack -d && cp src/index.html dist/index.html && webpack-dev-server --content-base src/ --inline --hot --port 8080",' >> package.json
+  echo '    "build:prod": "wbpack -p && cp src/index.html dist/index.html"' >> package.json
+  echo '  },' >> package.json
+  echo '  "keywords": [],' >> package.json
+  echo '  "author": "",' >> package.json
+  echo '  "license": "ISC"' >> package.json
+  echo '}' >> package.json
+else
+  npm init
+fi
+# --- Install prod dependencies -----------------------------------
+# if [ "$foo" == true ]; then
+#   npm install foo --save
+# fi
+npm install react react-dom --save
+
+# --- Install dev dependencies ------------------------------------
+# if [ "$foo" == true ]; then
+#   npm install foo --save
+# fi
+npm install webpack --save-dev
+npm install webpack-dev-server --save-dev
+npm install babel-core --save-dev
+npm install babel-loader --save-dev
+npm install babel-preset-es2016 --save-dev
+npm install babel-preset-stage-2 --save-dev
+npm install babel-preset-react --save-dev
+
+#-add-scripts-to-package.js----------------------------------------
+#sed -i '/"scripts":{/r ~/.uc/scrips.conf' package.json
+
+#-create-file:-webpack.config.js-----------------------------------
+touch webpack.config.js
+echo 'var webpack = require("webpack");' >> webpack.config.js
+echo 'var path = require("path");' >> webpack.config.js
+echo ' ' >> webpack.config.js
+echo 'var DIST_DIR = path.resolve(__dirname, "dist");' >> webpack.config.js
+echo 'var SRC_DIR = path.resolve(__dirname, "src");' >> webpack.config.js
+echo ' ' >> webpack.config.js
+echo 'var config = {' >> webpack.config.js
+echo '  entry: SRC_DIR + "/app/index.js",' >> webpack.config.js
+echo '  output: {' >> webpack.config.js
+echo '    path: DIST_DIR + "/app",' >> webpack.config.js
+echo '    filename: "bundle.js",' >> webpack.config.js
+echo '    publicPath: "/app/"' >> webpack.config.js
+echo '  },' >> webpack.config.js
+echo '  module: {' >> webpack.config.js
+echo '    loaders: [' >> webpack.config.js
+echo '      {' >> webpack.config.js
+echo '        test: /\.js?/,' >> webpack.config.js
+echo '        include: SRC_DIR,' >> webpack.config.js
+echo '        loader: "babel-loader",' >> webpack.config.js
+echo '        query: {' >> webpack.config.js
+echo '          presets: ["react", "es2016", "stage-2"]' >> webpack.config.js
+echo '        }' >> webpack.config.js
+echo '      }' >> webpack.config.js
+echo '    ]' >> webpack.config.js
+echo '  }' >> webpack.config.js
+echo '};' >> webpack.config.js
+echo ' ' >> webpack.config.js
+echo 'module.exports = config;' >> webpack.config.js
+
+#-create-file:-index.html------------------------------------------
+mkdir src && cd src
+touch index.html
+echo '<html>' >> index.html
+echo '  <head>' >> index.html
+echo '    <meta charset="UTF-8">' >> index.html
+echo '    <meta name="viewport"' >> index.html
+echo '          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">' >> index.html
+echo '    <title>'$param1'</title>' >> index.html
+echo '  </head>' >> index.html
+echo '  <body>' >> index.html
+echo '    <script src="/app/bundle.js"></script>' >> index.html
+echo '  </body>' >> index.html
+echo '</html>' >> index.html
+
+#-create-file:-index.js--------------------------------------------
 mkdir app && cd app
 touch index.js
-#-create-file:-index.js---------------------------------------------
-echo "import _ from 'lodash';" >> index.js
-echo " " >> index.js
-echo "function component () {" >> index.js
-echo "  var element = document.createElement('div');" >> index.js
-echo " " >> index.js
-echo "  /* lodash is required for the next line to work */" >> index.js
-echo "  element.innerHTML = _.join(['Hello','webpack'], ' ');" >> index.js
-echo " " >> index.js
-echo "  return element;" >> index.js
-echo "}" >> index.js
-echo " " >> index.js
-echo "document.body.appendChild(component());" >> index.js
-# -----------------------------------------------------------------
-cd ..
-touch index.html
-#-create-file:-index.html------------------------------------------
-echo "<html>" >> index.html
-echo "  <head>" >> index.html
-echo "    <title>$param1</title>" >> index.html
-echo "  </head>" >> index.html
-echo "  <body>" >> index.html
-echo "    <script src="dist/bundle.js"></script>" >> index.html
-echo "  </body>" >> index.html
-echo "</html>" >> index.html
-# -----------------------------------------------------------------
-./node_modules/.bin/webpack app/index.js dist/bundle.js
-xdg-open ./index.html
+echo 'console.log("It works!");' >> index.js
+
+#-build-and-run-dev-server-----------------------------------------
+cd ../..
+npm start
 # -----------------------------------------------------------------
 exit 0
